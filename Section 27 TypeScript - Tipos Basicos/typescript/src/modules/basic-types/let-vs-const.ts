9cab5b89196a13e4875b338d0baa13d82220cfef
Gabriel <gabrielhdantaspimentel@gmail.com>
2026-04-30 23:15:45 -0300

    Subindo aula 272 - Let vs Const

---
+++ Section 27 TypeScript - Tipos Basicos/typescript/src/modules/basic-types/let-vs-const.ts
@@ -0,0 +1,9 @@
+let phrase1 = 'DO IT'
+
+// const phrase2: 'JUST DO IT'  = 'JUST DO IT' // QUANDO É CONST O TS LÊ ASSIM
+
+const phrase2 = 'JUST DO IT'
+
+let phrase3 = 'MAKE YOUR DREAMS COME TRUE' as const
+
+export default () => {};
