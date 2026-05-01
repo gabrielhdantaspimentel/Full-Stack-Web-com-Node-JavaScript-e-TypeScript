4396517c1bc70274a7db79b2a0ea176745a7040c
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-04-30 22:07:34 -0300

    Finalizando aula 269 - Tipo Object - Index Signature

---
+++ Section 27 TypeScript - Tipos Basicos/typescript/src/modules/basic-types/index-signatures.ts
@@ -0,0 +1,41 @@
+type Movie = {
+    title: string;
+    year: number;
+    [key: string | number ] : string | number | boolean;
+}
+
+type Movies = {
+    [key: string] : Movie
+}
+
+
+let movies = {
+    movie1: {
+        title: 'A Origem',
+        year: 2010,
+        isFavorite: true,
+        genre: 'Ficção cientifica',
+        director: 'Christopher Nolan',
+    },
+    
+    movies2: {
+        title: 'Um sonho de Liberdade',
+        year: 1994,
+        isFavorite: true,
+        genre: 'Drama',
+        runtime: 142,
+    },
+
+    movie3: {
+        title: 'The Godfather',
+        year: 1972,
+        isFavorite: false,
+        genre: 'Crime',
+    },
+}
+
+export function showMovies(movies: Movies) {
+    console.log(movies)
+}
+
+showMovies(movies);
