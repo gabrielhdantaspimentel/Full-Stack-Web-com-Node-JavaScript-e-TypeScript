667e9712b1450a6458d59d23ccb530539b7b9834
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-05-05 20:44:35 -0300

    finalizada aula 279 - e atualização de configuração no tsconfig.json e no package.json

--- Section 27 TypeScript - Tipos Basicos/typescript/src/script.ts
+++ Section 27 TypeScript - Tipos Basicos/typescript/src/script.ts
@@ -1,7 +1,3 @@
-export function sum(a: number, b: number) {
-  console.log('Dentro do escopo do módulo');
-  return;
-  {
-    result: a + b;
-  }
+export function bootstrap () {
+  console.log('Script carregado com sucesso');
 }
