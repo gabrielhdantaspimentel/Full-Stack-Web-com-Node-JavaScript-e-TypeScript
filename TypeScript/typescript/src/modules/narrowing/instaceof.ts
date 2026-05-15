6b91a7692cc2ffa934f0eb59fad3e1e7ccd5fb44
Gabriel <gabrielhdantaspimentel@gmail.com>
2026-05-15 17:20:32 -0300

    Aula 296 finalizada - Verificando o tipo de objeto instaceof part 2

--- TypeScript/typescript/src/modules/narrowing/instaceof.ts
+++ TypeScript/typescript/src/modules/narrowing/instaceof.ts
@@ -14,10 +14,11 @@ export const bootstrap = (): void => {
         }
     }
 
-    class CheckingAccount {
+    class CheckingAccount extends BankAccount{
         private overdraftLimit: number;
 
-        constructor(overdraftLimit: number){
+        constructor(holder:string, balance: number,  overdraftLimit: number){
+            super(holder, balance)
             this.overdraftLimit = overdraftLimit
         }
 
@@ -26,27 +27,68 @@ export const bootstrap = (): void => {
         }
     }
 
-    const a = new BankAccount('Gabriel', 15000)
-    const b = new CheckingAccount(25000)
+    class SavingsAccount extends BankAccount {
+        private interestRate: number;
 
-    console.log(a)
-    console.log(typeof a)
-    console.log(a instanceof BankAccount )
-    console.log(a instanceof CheckingAccount )
-
-
-    function showDetails(account: BankAccount | CheckingAccount): void {
-        
-        if(account instanceof BankAccount){
-            console.log(account.getHolder())
-        } else if ( account instanceof CheckingAccount){
-            console.log(account.getOverdratLimit)
-        } else {
-            console.error("conta não identificada")
+        constructor(holder: string, balance: number, interestRate: number){
+            super(holder, balance)
+            this.interestRate = interestRate;
         }
 
+        public getInterestRate(): number {
+            return this.interestRate;
+        }
     }
 
-    showDetails(a)
-    showDetails(b)
+    // LISTA DE CONTAS BANCÁRIAS (LSP)
+    const accountList: BankAccount[] = [
+        new CheckingAccount('Alice', 1500, 300),
+        new SavingsAccount('Joao', 4000, 0.005),
+        new CheckingAccount('Maria', 10000, 3000),
+        new SavingsAccount('Pedro', 1000, 0.005),
+        
+    ]
+
+    
+    function processAccount(accounts: BankAccount[]): void{
+        accounts.forEach(account => {
+
+            if ( account instanceof CheckingAccount ){
+                console.log('Processando a conta corrente: ', account.getOverdratLimit())
+            } else if ( account instanceof SavingsAccount ){
+                console.log('Processando a conta poupança: ', account.getInterestRate())
+            }
+            console.log('-------------------------------')
+        })
+    }
+
+    processAccount(accountList)
+
+    /*  CONTEUDO DA AULA ANTERIOR
+    ////////////////////////////////////////////////         
+    ////////////////////////////////////////////////
+    // const a = new BankAccount('Gabriel', 15000)
+    // const b = new CheckingAccount(25000)
+
+    // console.log(a)
+    // console.log(typeof a)
+    // console.log(a instanceof BankAccount )
+    // console.log(a instanceof CheckingAccount )
+
+
+    // function showDetails(account: BankAccount | CheckingAccount): void {
+        
+    //     if(account instanceof BankAccount){
+    //         console.log(account.getHolder())
+    //     } else if ( account instanceof CheckingAccount){
+    //         console.log(account.getOverdratLimit)
+    //     } else {
+    //         console.error("conta não identificada")
+    //     }
+
+    // }
+
+    // showDetails(a)
+    // showDetails(b)
+     */
 }
