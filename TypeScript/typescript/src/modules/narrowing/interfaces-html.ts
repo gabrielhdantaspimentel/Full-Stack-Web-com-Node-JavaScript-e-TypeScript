823579dc61ec4d069d565bfb72eb76045c6d11ba
Gabriel <gabrielhdantaspimentel@gmail.com>
2026-05-15 16:38:51 -0300

    Finalizada aula 294 - Interface dos elementos HTML

---
+++ TypeScript/typescript/src/modules/narrowing/interfaces-html.ts
@@ -0,0 +1,15 @@
+export const bootstrap = (): void => {
+    const input = document.querySelector('.inputText') as HTMLInputElement
+    const video = document.querySelector('#promo') as HTMLVideoElement
+
+    input.addEventListener('click', (event: Event) => {
+        console.log("input clicado")
+    });
+
+    console.log('Filho de ', input.parentNode);
+    console.log('Element: ', input.id, input.className, input.tagName);
+    console.log('Arrastável: ', input.draggable);
+    console.log('Input: ', input.value);
+    console.log('Video: ', video.volume);
+
+}
