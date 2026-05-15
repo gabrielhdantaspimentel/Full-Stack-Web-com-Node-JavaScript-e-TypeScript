26710b7802f9ed8fa91d3ab06d5e42ec6a01953d
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-05-15 16:09:52 -0300

    finalizada aula 293 - Type Assertion as

---
+++ TypeScript/typescript/src/modules/narrowing/type-assertion.ts
@@ -0,0 +1,13 @@
+export const bootstrap = (): void => {
+    const body = document.querySelector('body') as HTMLBodyElement
+    const video = document.querySelector('#video') as HTMLVideoElement
+
+    const input = document.querySelector('.inputText') as HTMLInputElement
+
+    video.volume
+
+    if (input){
+    input.addEventListener('blur', (event: FocusEvent) => { 
+        alert('teste')})
+    }
+}
