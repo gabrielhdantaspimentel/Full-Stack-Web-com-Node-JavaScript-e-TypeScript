e25c7c38ea409734e0cc213ec7eecc78b23fc784
Gabriel <gabrielhdantaspimentel@gmail.com>
2026-05-19 20:31:35 -0300

    Aula 305 - Estendendo Interfaces - Extends

--- TypeScript/typescript/src/modules/interface/extends.ts
+++ TypeScript/typescript/src/modules/interface/extends.ts
@@ -1,57 +1,63 @@
 export const bootstrap = (): void => {
-    
-    interface PersonalInfo {
-        fullName: string;
-        email: string,
-        dateOfBirth?: Date;
-        sumary?: string;
+  interface PersonalInfo {
+    fullName: string;
+    email: string;
+    dateOfBirth?: Date;
+    sumary?: string;
+  }
+
+  interface Resume extends PersonalInfo {
+    skills: Skill[];
+    addSkill?: (skill: Skill) => boolean;
+  }
+
+  interface Skill {
+    name: string;
+    level: 'beginner' | 'intermediate' | 'advanced';
+  }
+
+  type Font = 'roboto' | 'open sans' | 'poppins';
+  type ColorScheme = 'light' | 'dark';
+  type Layout = 'one-column' | 'two-column';
+
+  interface Theme {
+    font: Font;
+    colorScheme: ColorScheme;
+    laryout: Layout;
+  }
+
+  class MyResume implements Resume {
+    constructor(
+      public fullName: string,
+      public email: string,
+      public skills: Skill[],
+      public font: Font,
+      public colorScheme: ColorScheme,
+      public laryout: Layout,
+    ) {}
+
+    addSkill(skill: Skill): boolean {
+      const initialLenght = this.skills.length;
+      this.skills.push(skill);
+
+      return this.skills.length > initialLenght;
     }
-    
-    interface Resume extends PersonalInfo {
-        skills: Skill[], 
-        addSkill?: (skill: Skill) => boolean
-    } 
+  }
 
-    interface Skill {
-        name: string,
-        level: 'beginner' | 'intermediate' | 'advanced'
-    }
+  const myResume = new MyResume(
+    'Gabriel Henrique Dantas Pimentel',
+    'gabriel@email.com',
+    [],
+    "roboto",
+    "dark",
+    "two-column",
+  );
 
-    type Font
+  myResume.addSkill({ name: 'JavaScript', level: 'beginner' });
+  myResume.addSkill({ name: 'TypeScript', level: 'intermediate' });
+  console.log(myResume);
 
-    interface Theme {
-        font: 'roboto' | 'open sans';
-        colorScheme: 'light' | 'dark';
-        laryout: 'one-column' | 'two-column'
-    }
-
-    class MyResume implements Resume {
-        constructor(
-            public fullName: string,
-            public email: string,
-            public skills: Skill[],
-            public font: 'roboto' | 'open sans';  
-        ){}
-
-        addSkill(skill: Skill): boolean{
-            const initialLenght = this.skills.length
-            this.skills.push(skill)
-
-            return this.skills.length > initialLenght
-        }
-    }
-
-    const myResume = new MyResume(
-        'Gabriel Henrique Dantas Pimentel', 
-        'gabriel@email.com', 
-        []
-    )
-
-    myResume.addSkill({name: 'JavaScript', level: 'beginner'})
-    myResume.addSkill({name: 'TypeScript', level: 'intermediate'})
-    console.log(myResume)
-
-    /*
+  /*
     const MyResume: Resume = {
         fullName: 'Gabriel Henrique Dantas Pimentel',
         email: 'gabriel@email.com',
@@ -63,5 +69,4 @@ export const bootstrap = (): void => {
 
     console.log(MyResume)
     */
-}
-     
+};
