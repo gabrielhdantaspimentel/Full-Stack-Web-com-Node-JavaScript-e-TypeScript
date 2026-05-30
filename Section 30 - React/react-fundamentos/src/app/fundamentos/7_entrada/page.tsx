d3dd48d34fed693fabf590bc9cb35fc25ee789b5
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-05-30 14:04:45 -0300

    Aula 330 - Criando caixa de texto

---
+++ Section 30 - React/react-fundamentos/src/app/fundamentos/7_entrada/page.tsx
@@ -0,0 +1,11 @@
+export default function Entrada(){
+    return (
+        <div className="p-2">
+            <h1>Entrada de dados</h1>
+            <input 
+            value={"Digite algo"}
+            className="campo-texto" 
+            type="text" />
+        </div>
+    )
+}
