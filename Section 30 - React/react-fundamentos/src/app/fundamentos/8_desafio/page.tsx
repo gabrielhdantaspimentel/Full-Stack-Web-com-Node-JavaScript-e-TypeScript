0fcbea5b4ab585b5e842f389f1fe8d429550bd67
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-05-30 18:15:07 -0300

    Aula 333 - [Projeto] Calculando IMC - (Só a versão do curso)

--- Section 30 - React/react-fundamentos/src/app/fundamentos/8_desafio/page.tsx
+++ Section 30 - React/react-fundamentos/src/app/fundamentos/8_desafio/page.tsx
@@ -4,7 +4,25 @@ import { useState } from "react"
 
 export default function Entrada(){
 
-     
+    const [peso, setPeso] = useState('')
+    const [altura, setAltura] = useState('')
+    const [resultado, setResultado] = useState('')
+
+     function calcularImc(){
+        const p = parseFloat(peso)
+        const a = parseFloat(altura)
+
+        const imc = p / (a * a)
+
+        if ( imc <= 18 ){
+            setResultado('Abaixo do peso')
+        } else if ( imc > 18 && imc < 25 ){
+            setResultado('Peso normal')
+        } else {
+            setResultado('Sobrepeso')
+        }
+
+     }
 
     return (
         <div className="p-2">
@@ -13,6 +31,8 @@ export default function Entrada(){
 
             <p>Digite seu peso</p>
             <input 
+            value={peso}
+            onChange={e=>setPeso(e.target.value)}
             placeholder="ex: 90"
             className="campo-texto"
             type="text"
@@ -20,25 +40,28 @@ export default function Entrada(){
 
             <br />
 
-            <p>Digite seu peso</p>
+            <p>Digite sua altura</p>
             <input 
+            value={altura}
+            onChange={e=>setAltura(e.target.value)}
             placeholder="ex: 1.83"
             className="campo-texto"
             type="text"
-            /> KG
+            /> metros
 
             <br />
             <br />
 
             <button
+            onClick={calcularImc}
             className="botao"
             >
                 Calcular
             </button>
 
             <br />
-            
-            <h4>Valor digitado: {}</h4>
+
+            <h4>Valor digitado: {resultado}</h4>
 
         </div>
     )
