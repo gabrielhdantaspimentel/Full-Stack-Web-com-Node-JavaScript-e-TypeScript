8d6bba7665582e965a1b76583afa987c9208d54f
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-06-03 15:30:55 -0300

    Aula 336 - Criando componentes --- Módulo de React finalizado

---
+++ Section 30 - React/react-fundamentos/src/app/fundamentos/7_componentes/page.tsx
@@ -0,0 +1,9 @@
+export default function Aplicacao(){
+    return (
+        <div className="p-2">
+            <button className="btn-sucesso">Sucesso</button>
+            <button className="btn-info">Informações</button>
+            <button className="btn-erro">Erro</button>
+        </div>
+    )
+}
