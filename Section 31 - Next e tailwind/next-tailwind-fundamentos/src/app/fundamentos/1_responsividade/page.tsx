62da404054ae21f7fd2098d0c79066e81665dc01
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-06-04 20:50:02 -0300

    Aula 345 - Prática com responsividade - Breakpoints

---
+++ Section 31 - Next e tailwind/next-tailwind-fundamentos/src/app/fundamentos/1_responsividade/page.tsx
@@ -0,0 +1,9 @@
+export default function Responsividade(){
+    return (
+        <div>
+            <h1 className="bg-green-400 md:bg-yellow-400 lg:bg-orange-500 xl:bg-red-500 2xl:bg-black 2xl:text-white">
+                Responsividade
+            </h1>
+        </div>
+    )
+}
