9c6e242114c972bab6b63296ea7947693a805d15
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-06-04 20:41:24 -0300

    Aula 345 - Flex - Direções e espaçamentos

---
+++ Section 31 - Next e tailwind/next-tailwind-fundamentos/src/app/fundamentos/11_flexbox/page.tsx
@@ -0,0 +1,11 @@
+export default function Aplicacao(){
+    return (
+
+    // flex flex-row-reverse flex-wrap
+    <div className='border-4 flex flex-row gap-2'>
+        <div className="caixa basis-64">Caixa 1</div>
+        <div className="caixa basis-96">Caixa 2</div>
+        <div className="caixa basis-64">Caixa 3</div>
+    </div>
+    )
+}
