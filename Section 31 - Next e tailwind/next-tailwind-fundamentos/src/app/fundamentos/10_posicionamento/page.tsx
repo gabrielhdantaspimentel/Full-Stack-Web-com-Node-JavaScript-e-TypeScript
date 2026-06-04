b6b4dd0557b2bf715d2f3b5d9e373a648be78201
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-06-04 20:10:15 -0300

    AUla 340 - Configuração inicial (já foi feito no commit anterior antecipado) --- Aula 341 - Posicionamentos Static e Relative

---
+++ Section 31 - Next e tailwind/next-tailwind-fundamentos/src/app/fundamentos/10_posicionamento/page.tsx
@@ -0,0 +1,32 @@
+export default function Aplicacao(){
+    return (
+    <div className='border p-3'>
+
+        <p className="bg-orange-400 m-2">
+            ITEM DE DESTAQUE Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Voluptas, iusto quos placeat aspernatur suscipit
+            cum dignissimos culpa nam quo. 
+            Nostrum, harum error eaque beatae facilis nisi. 
+            Iste asperiores consectetur molestiae.
+        </p>
+
+        {/* <p className="bg-purole-400 m-2 relative top-4 left-4 bottom-4 right-4"> */}
+        <p className="bg-purple-400 m-2 relative left-2">
+            ITEM 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Voluptas, iusto quos placeat aspernatur suscipit
+            cum dignissimos culpa nam quo. 
+            Nostrum, harum error eaque beatae facilis nisi. 
+            Iste asperiores consectetur molestiae.
+        </p>
+
+        <p className="bg-gray-400 m-2">
+            ITEM 2 Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Voluptas, iusto quos placeat aspernatur suscipit
+            cum dignissimos culpa nam quo. 
+            Nostrum, harum error eaque beatae facilis nisi. 
+            Iste asperiores consectetur molestiae.
+        </p>
+
+    </div>
+    )
+}
