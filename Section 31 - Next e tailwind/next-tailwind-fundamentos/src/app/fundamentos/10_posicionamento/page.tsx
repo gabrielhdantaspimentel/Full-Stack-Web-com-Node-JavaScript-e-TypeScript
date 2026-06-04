1feae809e60302c1d602ee870bda3691baa611d5
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-06-04 20:25:18 -0300

    Aula 323 Posicionamentos - Absolute, fixed e stick

--- Section 31 - Next e tailwind/next-tailwind-fundamentos/src/app/fundamentos/10_posicionamento/page.tsx
+++ Section 31 - Next e tailwind/next-tailwind-fundamentos/src/app/fundamentos/10_posicionamento/page.tsx
@@ -2,7 +2,10 @@ export default function Aplicacao(){
     return (
     <div className='border p-3'>
 
-        <p className="bg-orange-400 m-2">
+
+        {/* <p className="bg-orange-400 m-2 absolute bottom-2"> */}
+        {/* <p className="bg-orange-400 m-2 fixed bottom-2"> */}
+        <p className="bg-orange-400 m-2 sticky top-4 z-10">
             ITEM DE DESTAQUE Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
             Voluptas, iusto quos placeat aspernatur suscipit
             cum dignissimos culpa nam quo. 
@@ -11,7 +14,7 @@ export default function Aplicacao(){
         </p>
 
         {/* <p className="bg-purole-400 m-2 relative top-4 left-4 bottom-4 right-4"> */}
-        <p className="bg-purple-400 m-2 relative left-2">
+        <p className="bg-purple-400 m-2 sticky top-4 z-20">
             ITEM 1 Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
             Voluptas, iusto quos placeat aspernatur suscipit
             cum dignissimos culpa nam quo. 
@@ -19,7 +22,7 @@ export default function Aplicacao(){
             Iste asperiores consectetur molestiae.
         </p>
 
-        <p className="bg-gray-400 m-2">
+        <p className="bg-gray-400 m-2 sticky top-4 z-30">
             ITEM 2 Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
             Voluptas, iusto quos placeat aspernatur suscipit
             cum dignissimos culpa nam quo. 
