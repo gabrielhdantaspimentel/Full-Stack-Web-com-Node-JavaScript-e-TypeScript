49a77abca313baf6f466df70346f2e3e587c8de8
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-06-03 23:17:37 -0300

    Aula 337 - Colunas

---
+++ Section 30 - React/react-fundamentos/src/app/fundamentos/8_colunas_tail/page.tsx
@@ -0,0 +1,13 @@
+export default function Aplicacao(){
+    return (
+        // <div className="p-2 columns-3">
+        <div className="p-2 columns-1 md:columns-2 lg:columns-3">
+            <div className="w-32 h-32 border bg-blue-400">Item1</div>
+            <div className="w-32 h-32 border bg-blue-400">Item2</div>
+            <div className="w-32 h-32 border bg-blue-400">Item3</div>
+            <div className="w-32 h-32 border bg-blue-400">Item4</div>
+            <div className="w-32 h-32 border bg-blue-400">Item5</div>
+            <div className="w-32 h-32 border bg-blue-400">Item6</div>
+        </div>
+    )
+} 
