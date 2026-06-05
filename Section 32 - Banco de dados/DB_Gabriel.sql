0891eed450a84a5523ec5b477e66dfde2fae1bb0
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-06-05 12:34:58 -0300

    Aula 362 - Criando tabelas

---
+++ Section 32 - Banco de dados/DB_Gabriel.sql
@@ -0,0 +1,24 @@
+/*
+CREATE DATABASE facebook;
+create database facebook2;
+DROP DATABASE FACEBOOK;
+USE facebook;
+
+CREATE TABLE usuarios 
+(
+	email VARCHAR(39),
+    senha VARCHAR(8)
+);
+
+DESCRIBE usuarios;
+
+DROP TABLE usuarios;
+
+*/
+
+CREATE TABLE usuarios 
+(
+	email VARCHAR(39),
+    senha VARCHAR(8)
+);
+
