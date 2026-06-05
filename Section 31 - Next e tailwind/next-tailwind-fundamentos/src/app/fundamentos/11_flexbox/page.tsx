8121ed7dae60734f3748be6c548d6f2b67572943
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-06-04 21:25:09 -0300

    Aula 349 - Flex Redimensionamento

--- Section 31 - Next e tailwind/next-tailwind-fundamentos/src/app/fundamentos/11_flexbox/page.tsx
+++ Section 31 - Next e tailwind/next-tailwind-fundamentos/src/app/fundamentos/11_flexbox/page.tsx
@@ -3,10 +3,11 @@ export default function Aplicacao(){
 
     // flex flex-row-reverse flex-wrap
     // order-2 grow justify-between
-    <div className='border-4 p-3 h-screen flex flex-row gap-2 justify-evenly items-start'>
-        <div className="caixa h-36">Caixa 1</div>
-        <div className="caixa">Caixa 2</div>
-        <div className="caixa">Caixa 3</div>
+    // border-4 p-3 h-screen flex flex-row gap-2 justify-evenly items-start
+    <div className='border-4 p-3 flex gap-2'>
+        <div className="caixa w-48 grow-0 shrink-0">Caixa 1</div>
+        <div className="caixa w-72 grow shrink">Caixa 2</div>
+        <div className="caixa w-96 grow-0 shrink-0">Caixa 3</div>
     </div>
     )
 }
