cf70fbaf10b7d60686e3a0d492eb11c9954c478e
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-06-04 21:17:06 -0300

    Aula 348 - Tipografia

---
+++ Section 31 - Next e tailwind/next-tailwind-fundamentos/src/app/fundamentos/2_tipografia/page.tsx
@@ -0,0 +1,27 @@
+export default function Responsividade(){
+    return (
+        <div>
+
+            <h1 className="
+            text-[1-rem] 
+            md:text-[2rem] 
+            lg:text-[3rem]
+            ">Titulo 05</h1>
+
+            <h1 className="font-sans">Titulo 01</h1>
+            <h1 className="font-serif">Titulo 02</h1>
+            <h1 className="font-mono">Titulo 03</h1>
+            <h1 className="font-2xl">Titulo 04</h1>
+            <h1 className="italic">Titulo 06</h1>
+            <h1 className="font-thin">Titulo 06</h1>
+            <h1 className="font-thin tracking-wider">Titulo 06</h1>
+
+            
+            <h1 className="text-left">Esquerda</h1>
+            <h1 className="text-center">Cetro</h1>
+            <h1 className="text-right">Direita</h1>
+            
+            
+        </div>
+    )
+}
