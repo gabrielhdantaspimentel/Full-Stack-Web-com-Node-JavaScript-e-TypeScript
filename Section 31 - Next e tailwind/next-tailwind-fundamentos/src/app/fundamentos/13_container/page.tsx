1e7dec174c5314cbe286d1ae4d22510953524bde
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-06-04 22:33:08 -0300

    Aula 357 - Container --- Final Section 31 - Next e Tailwind --- Correção de diretórios

---
+++ Section 31 - Next e tailwind/next-tailwind-fundamentos/src/app/fundamentos/13_container/page.tsx
@@ -0,0 +1,21 @@
+
+export default function Aplicacao(){
+    return (
+
+        <div className="container mx-auto bg-green-400 border-3">
+
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta deleniti optio ipsam iure ut vitae magni iste ratione, animi illo quis similique voluptatum nesciunt voluptates perspiciatis corrupti ea enim! Neque!
+            </p>
+
+            <div className="border-4 p-3 grid gap-2 grid-cols-4">
+                <div className="caixa">Caixa 1</div>        
+                <div className="caixa">Caixa 2</div>        
+                <div className="caixa">Caixa 3</div>     
+            </div>
+
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta deleniti optio ipsam iure ut vitae magni iste ratione, animi illo quis similique voluptatum nesciunt voluptates perspiciatis corrupti ea enim! Neque!</p>
+
+        </div>
+    )
+}
