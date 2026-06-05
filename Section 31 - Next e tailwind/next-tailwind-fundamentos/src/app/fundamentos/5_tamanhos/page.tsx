e893916c5228ff7a255fd735f4d4b9fc6d057fff
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-06-04 22:09:11 -0300

    Aula 354 - Tamanhos - Largura e altura

---
+++ Section 31 - Next e tailwind/next-tailwind-fundamentos/src/app/fundamentos/5_tamanhos/page.tsx
@@ -0,0 +1,57 @@
+export function AplicacaoLargura(){
+    return (
+        <div className="flex">
+            {/* w-96,w-[500px],   */}
+            <div className="
+                bg-orange-400 w-1/5
+            ">
+                Lorem ipsum dolor sit 1
+            </div>
+
+            <div className="
+                bg-red-400 w-4/5
+            ">
+                Lorem ipsum dolor sit 2
+            </div>
+
+        </div>
+    )
+}
+
+export function AplicacaoLargura2(){
+    return (
+        <div>
+            {/* w-96,w-[500px],   */}
+            <div className="
+                bg-orange-400 w-screen
+            ">
+                Lorem ipsum dolor sit 1
+            </div>
+
+            <div className="bg-purple-500 size-40">Tamanho</div>
+
+            
+
+            <div className="bg-purple-500 size-40">Tamanho</div>
+
+        </div>
+    )
+}
+
+export default function Aplicacao(){
+    return (
+        <div className="bg-blue-300 h-screen">
+            {/* h-96,h-[500px],   */}
+            
+            <div className="bg-purple-500 h-1/2">
+                Item 1
+            </div>
+
+            <div className="bg-red-500 h-1/2">
+                Item 2
+            </div>
+            
+
+        </div>
+    )
+}
