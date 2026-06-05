e1035f5105133129106550e8c884947dcb0fcde9
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-06-04 22:15:24 -0300

    Aula 356 - Bordas

---
+++ Section 31 - Next e tailwind/next-tailwind-fundamentos/src/app/fundamentos/6_bordas/page.tsx
@@ -0,0 +1,30 @@
+
+export default function Aplicacao(){
+    return (
+        <div>
+            {/* border-2 border-orange-500, border-l-2  */}
+            <div className="
+            m-8 
+            border-2 
+            rounded-xl
+            border-double
+             border-orange-500">
+                Lorem ipsum dolor sit amet consectetur 
+                adipisicing elit. Quos, vitae voluptas! 
+                Fugit maxime pariatur tempore aut, distinctio 
+                expedita doloremque, fuga reprehenderit 
+                laboriosam voluptatibus dolore mollitia 
+                quisquam maiores quibusdam debitis facilis.
+            </div> 
+
+            <button className="
+            rounded-[5px] 
+            bg-blue-500 
+            px-4
+             text-white
+             hover:bg-blue-200
+            ">Save Changes</button>
+
+        </div>
+    )
+}
