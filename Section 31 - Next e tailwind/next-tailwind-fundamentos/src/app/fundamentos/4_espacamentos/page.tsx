962d803842935dfb5adaacd86fdb5147963cf088
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-06-04 22:00:00 -0300

    Aula 352 - Espaçamentos - padding e margin

---
+++ Section 31 - Next e tailwind/next-tailwind-fundamentos/src/app/fundamentos/4_espacamentos/page.tsx
@@ -0,0 +1,38 @@
+export function Aplicacao2(){
+    return (
+        <div>
+            {/* 
+            pl-6, px-6, py-6, p-[40px]
+            */}
+            <div className="bg-blue-300 p-2 md:p-4 lg:p-6">
+                Lorem ipsum dolor sit, amet consectetur 
+                adipisicing elit. Sit in a accusamus nulla 
+                tempora illo beatae nisi blanditiis ullam optio, 
+                eius ex voluptatum quasi vel? Accusamus est eaque 
+                quis vel?
+            </div>
+
+        </div>
+    )
+}
+
+export default function Aplicacao(){
+    return (
+        <div>
+            {/* 
+            mt-8
+            */}
+            {/* <div className="bg-blue-300 m-[20px]"> */}
+            <div className="bg-blue-300 m-5]">
+                Lorem ipsum dolor sit, amet consectetur 
+                adipisicing elit. Sit in a accusamus nulla 
+                tempora illo beatae nisi blanditiis ullam optio, 
+                eius ex voluptatum quasi vel? Accusamus est eaque 
+                quis vel?
+            </div>
+
+            <h1 className="bg-red-500">teste</h1>
+
+        </div>
+    )
+}
