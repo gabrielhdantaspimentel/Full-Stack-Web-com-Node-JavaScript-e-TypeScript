b40a5329114ec57f50353ade65e737c4e21d6f84
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-06-08 21:23:10 -0300

    Aula 372 Consultando registros - SELECT - Prática

--- Section 32 - Banco de dados/atualizando_e_excluindo_registros.sql
+++ Section 32 - Banco de dados/atualizando_e_excluindo_registros.sql
@@ -5,10 +5,28 @@ CREATE TABLE usuarios
     senha VARCHAR(12)
 );
 
-INSERT INTO usuarios(nome, usuario, senha)
-		VALUES ("Gabriel Pimentel", "gabriel.pimentel", "senha123");
+INSERT INTO usuarios(nome, usuario, senha, idade)
+		VALUES ("Gabriel Pimentel", "gabriel.pimentel", "senha123", 28);
         
-INSERT INTO usuarios(nome, usuario, senha)
-		VALUES ("Brenda Vieira", "brenda.vieira", "123456");usuarios
+INSERT INTO usuarios(nome, usuario, senha, idade)
+		VALUES ("Brenda Vieira", "brenda.vieira", "123456", 26);
         
+DELETE FROM usuarios WHERE usuario = 'brenda.vieira';
+
+UPDATE usuarios SET 
+	senha = '56789',
+    nome = 'Brenda Vieira Pimentel'
+    WHERE usuario = 'brenda.vieira';
+
+ALTER TABLE usuarios
+	ADD COLUMN idade TINYINT NOT NULL DEFAULT 0 AFTER senha;
+    
+ALTER TABLE usuarios
+	DROP COLUMN idade;
+    
+UPDATE usuarios SET
+	idade = '18'
+    WHERE usuario = 'brenda.vieira';
+
 DROP TABLE usuarios;
+
---
+++ Section 32 - Banco de dados/consultando_registros.sql
@@ -0,0 +1,8 @@
+CREATE TABLE vendas
+(
+	usuario VARCHAR(40),
+	produto VARCHAR(60),
+	preco DECIMAL(15, 2),
+	quantidade INT,
+	categoria VARCHAR(60)
+);
