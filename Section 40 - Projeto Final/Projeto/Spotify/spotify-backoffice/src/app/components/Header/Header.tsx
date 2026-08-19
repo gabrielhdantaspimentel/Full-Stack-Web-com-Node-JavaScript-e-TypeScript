c8e898eb780fb04d721de62a144d594a8c280747
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-08-19 12:55:50 -0300

    Aula 532 - Adicionando o Sass ao Projeto

--- Section 40 - Projeto Final/Projeto/Spotify/spotify-backoffice/src/app/components/Header/Header.tsx
+++ Section 40 - Projeto Final/Projeto/Spotify/spotify-backoffice/src/app/components/Header/Header.tsx
@@ -1,6 +1,6 @@
-import styles from './Header.module.css'
+import styles from './Header.module.scss'
 console.log(styles)
 
 export default function Header(){
-    return <div className={`header ${styles.header}`}> Logo | Menu</div>
+    return <div className={`header ${styles.header}`}> Logo | <a href="#">Link</a> </div>
 }
