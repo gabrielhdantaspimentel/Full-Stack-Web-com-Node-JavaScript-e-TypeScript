1afd48222580999dfd90287d8e7f4fb6aaaac6ed
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-08-19 15:13:13 -0300

    Aula 538 - Recebendo Parâmetros via Query String

--- Section 40 - Projeto Final/Projeto/Spotify/spotify-backoffice/src/app/(admin)/bands/[id]/page.tsx
+++ Section 40 - Projeto Final/Projeto/Spotify/spotify-backoffice/src/app/(admin)/bands/[id]/page.tsx
@@ -1,8 +1,19 @@
 'use client'
 
-import { useParams } from "next/navigation";
+import { useParams, useSearchParams } from "next/navigation";
 
 export default function Page(){
     const params = useParams<{ id: string }>()
-    return <h1>Banda ID ... {params.id} </h1>
+    const searchParams = useSearchParams()
+    const mode = searchParams.get('mode')
+    const showTitle = searchParams.get('showTitle')
+    const anotherParam = searchParams.get('anotherParam')
+    return (
+        <>
+        <h1>Banda ID ... {params.id} </h1>
+        <h2>Mode: {mode}</h2>
+        <h2>showTitle: {showTitle}</h2>
+        <h2>Outro Parametro: {anotherParam}</h2>
+        </>
+    )
 }
--- Section 40 - Projeto Final/Projeto/Spotify/spotify-backoffice/src/app/(admin)/tracks/[trackID]/page.tsx
+++ Section 40 - Projeto Final/Projeto/Spotify/spotify-backoffice/src/app/(admin)/tracks/[trackID]/page.tsx
@@ -1,11 +1,27 @@
 interface Props {
-    params: {
+        params: {
         trackId: string
+    },
+    searchParams: {
+        mode: string,
+        showTitle: string,
+        anotherParam: string
     }
 }
 
-export default async function Page({params}: Props){
+export default async function Page({ params,  searchParams }: Props){
     const p = await params
-    console.log(p)
-    return <h1>Música ID {p.trackId}</h1>
+    const trackId = p.trackId
+    const sP = await searchParams
+    const mode = sP.mode;
+    const showTitle = sP.showTitle
+    const anotherParam = sP.anotherParam
+    return (
+        <>
+        <h1>Música ID: {p.trackId}</h1>
+        <h1>mode: {mode}</h1>
+        <h1>showTitle: {showTitle}</h1>
+        <h1>anotherParam: {anotherParam}</h1>
+        </>
+    )
 }
