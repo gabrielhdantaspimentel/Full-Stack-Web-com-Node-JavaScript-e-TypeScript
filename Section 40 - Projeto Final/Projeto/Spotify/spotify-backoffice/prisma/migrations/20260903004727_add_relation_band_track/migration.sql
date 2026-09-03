caf5f839960e5fd52448d9af0cc233c023bb379f
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-09-02 22:10:54 -0300

    Aula 558 - Prisma ORM - Relacionamento One to Many (1:N)

---
+++ Section 40 - Projeto Final/Projeto/Spotify/spotify-backoffice/prisma/migrations/20260903004727_add_relation_band_track/migration.sql
@@ -0,0 +1,43 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('active', 'inactive');
+
+-- CreateTable
+CREATE TABLE "bands" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT,
+    "status" "Status" NOT NULL DEFAULT 'active',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "bands_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tracks" (
+    "id" UUID NOT NULL,
+    "title" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "duration_in_seconds" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "band_id" UUID NOT NULL,
+
+    CONSTRAINT "tracks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "bands_name_key" ON "bands"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "bands_slug_key" ON "bands"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tracks_slug_key" ON "tracks"("slug");
+
+-- CreateIndex
+CREATE INDEX "tracks_band_id_idx" ON "tracks"("band_id");
+
+-- AddForeignKey
+ALTER TABLE "tracks" ADD CONSTRAINT "tracks_band_id_fkey" FOREIGN KEY ("band_id") REFERENCES "bands"("id") ON DELETE CASCADE ON UPDATE CASCADE;
