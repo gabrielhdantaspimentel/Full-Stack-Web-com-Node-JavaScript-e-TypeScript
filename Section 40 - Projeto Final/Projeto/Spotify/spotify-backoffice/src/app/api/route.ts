c0df18d212ac16ec6c65bfd381f67569bebb5e13
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-09-17 19:43:10 -0300

    Aula 562 - API (Route Handlers) - Implementando Route Handlers para os métodos HTTP

---
+++ Section 40 - Projeto Final/Projeto/Spotify/spotify-backoffice/src/app/api/route.ts
@@ -0,0 +1,27 @@
+export async function GET() {
+  return Response.json({ msg: "API Rest - Método GET" });
+}
+
+export async function POST() {
+  return Response.json({ msg: "API Rest - Método POST" });
+}
+
+export async function PUT() {
+  return Response.json({ msg: "API Rest - Método PUT" });
+}
+
+export async function PATCH() {
+  return Response.json({ msg: "API Rest - Método PATCH" });
+}
+
+export async function DELETE() {
+  return Response.json({ msg: "API Rest - Método DELETE" });
+}
+
+export async function HEAD() {
+  return Response.json({ msg: "API Rest - Método HEAD" });
+}
+
+export async function OPTIONS() {
+  return Response.json({ msg: "API Rest - Método OPTIONS" });
+}
