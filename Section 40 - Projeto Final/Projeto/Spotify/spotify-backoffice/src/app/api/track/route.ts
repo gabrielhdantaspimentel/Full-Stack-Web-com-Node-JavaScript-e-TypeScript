567efaa10946c46f1687c85f3544d9cf0d523ab0
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-09-17 20:26:07 -0300

    Aula 564 - API (Route Handlers) - Separando Recursos

---
+++ Section 40 - Projeto Final/Projeto/Spotify/spotify-backoffice/src/app/api/track/route.ts
@@ -0,0 +1,30 @@
+import prisma from "../../../../lib/prisma";
+
+export async function GET() {
+  const items = await prisma.track.findMany();
+  return Response.json(items);
+}
+
+export async function POST() {
+  return Response.json({ msg: "API Rest - Método POST" });
+}
+
+export async function PUT() {
+  return Response.json({ msg: "API Rest - Método PUT" });
+}
+
+export async function PATCH() {
+  return Response.json({ msg: "API Rest - Método PATCH" });
+}
+
+export async function DELETE() {
+  return Response.json({ msg: "API Rest - Método DELETE" });
+}
+
+export async function HEAD() {
+  return Response.json({ msg: "API Rest - Método HEAD" });
+}
+
+export async function OPTIONS() {
+  return Response.json({ msg: "API Rest - Método OPTIONS" });
+}
