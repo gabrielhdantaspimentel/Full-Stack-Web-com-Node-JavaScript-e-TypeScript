92b412d3794a91faa71e5fffc343cb06dde8758b
Gabriel Pimentel <gabrielhdantaspimentel@gmail.com>
2026-09-17 21:47:36 -0300

    Aula 568 - API (Route Handlers)  Content-Type - multipart/form-data

--- Section 40 - Projeto Final/Projeto/Spotify/spotify-backoffice/src/app/api/band/route.ts
+++ Section 40 - Projeto Final/Projeto/Spotify/spotify-backoffice/src/app/api/band/route.ts
@@ -1,4 +1,6 @@
+import path from "path";
 import prisma from "../../../../lib/prisma";
+import { mkdir, writeFile } from "fs/promises";
 
 export async function GET() {
   const bands = await prisma.band.findMany();
@@ -6,27 +8,48 @@ export async function GET() {
 }
 
 export async function POST(request: Request) {
-  const data = await request.json();
-  console.log(data);
-  return Response.json({ msg: "Dados de banda recebidos com sucesso!", data });
+  const formData = await request.formData();
+
+  const file = formData.get("cover");
+
+  if (!(file instanceof File)) {
+    return Response.json(
+      { error: "Arquivo não enviado ou inválido" },
+      { status: 400 },
+    );
+  }
+
+  const arrayBuffer = await file.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+
+  const uploadDir = path.join(process.cwd(), "public", "uploads");
+  await mkdir(uploadDir, { recursive: true });
+
+  const filePath = path.join(uploadDir, file.name);
+  await writeFile(filePath, buffer);
+
+  return Response.json({
+    msg: "Dados recebidos com sucesso!",
+    filePath: `/uploads/${file.name}`,
+  });
 }
 
-export async function PUT() {
+export function PUT() {
   return Response.json({ msg: "API Rest - Método PUT" });
 }
 
-export async function PATCH() {
+export function PATCH() {
   return Response.json({ msg: "API Rest - Método PATCH" });
 }
 
-export async function DELETE() {
+export function DELETE() {
   return Response.json({ msg: "API Rest - Método DELETE" });
 }
 
-export async function HEAD() {
+export function HEAD() {
   return Response.json({ msg: "API Rest - Método HEAD" });
 }
 
-export async function OPTIONS() {
+export function OPTIONS() {
   return Response.json({ msg: "API Rest - Método OPTIONS" });
 }
